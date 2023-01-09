6803f83d790f059de6e8850451846bb9f8387bd2
trancu900 <pet.cu20791@gmail.com>
2023-01-09 17:21:50 +0700

    fisrt commit

---
+++ src/main/java/vn/tg/ohana/OhanaApplication.java
@@ -0,0 +1,13 @@
+package vn.tg.ohana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OhanaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OhanaApplication.class, args);
+    }
+
+}
