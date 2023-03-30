0cba42a9d45441446398af39decb08ea954a3c36
smeehui <nguyenvanquanghuy99@gmail.com>
2023-03-30 17:29:35 +0700

    huy merger user

--- ohana-core/src/main/java/vn/ohana/user/dto/UserResult.java
+++ ohana-core/src/main/java/vn/ohana/user/dto/UserResult.java
@@ -1,10 +1,35 @@
 package vn.ohana.user.dto;
 
 import lombok.Data;
+import vn.ohana.entities.Role;
+import vn.ohana.entities.UserStatus;
 
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.time.Instant;
 
 @Data
 public class UserResult {
+    private Long id;
 
+    private String fullName;
+
+    private String email;
+    private String phone;
+
+    private String description;
+
+    private String address;
+
+    private Role role;
+
+    private String password;
+
+    private Instant lastLogin;
+
+    private String thumbnailId;
+
+    private UserStatus status;
 }
--- ohana-core/src/main/java/vn/ohana/user/dto/UserUpdateParam.java
+++ ohana-core/src/main/java/vn/ohana/user/dto/UserUpdateParam.java
@@ -1,10 +1,33 @@
 package vn.ohana.user.dto;
 
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
+import vn.ohana.entities.Role;
+import vn.ohana.entities.UserStatus;
 
-@Getter
-@Setter
+import java.time.Instant;
+
+@Data
 public class UserUpdateParam extends BaseUser {
     private Long id;
+
+    private String fullName;
+
+    private String email;
+    private String phone;
+
+    private String description;
+
+    private String address;
+
+    private Role role;
+
+    private String password;
+
+    private Instant lastLogin;
+
+    private String thumbnailId;
+
+    private UserStatus status;
 }
