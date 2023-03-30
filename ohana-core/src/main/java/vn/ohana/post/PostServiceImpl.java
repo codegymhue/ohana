9b433887a1084dd56bc7e6def25aeba03b370297
TrongAnh16 <anhtrduong@gmail.com>
2023-03-30 16:23:07 +0700

    Cu fix

--- ohana-core/src/main/java/vn/ohana/post/PostServiceImpl.java
+++ ohana-core/src/main/java/vn/ohana/post/PostServiceImpl.java
@@ -4,12 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import vn.ohana.entities.Post;
-import vn.ohana.post.dto.PostCreateParam;
 import vn.ohana.post.dto.PostResult;
-import vn.ohana.post.dto.PostUpdateParam;
 import vn.ohana.utility.UtilityRepository;
+import vn.ohana.utility.UtilityService;
 
-import java.io.IOException;
+import java.awt.print.Pageable;
 import java.util.List;
 
 
@@ -25,14 +24,15 @@ public class PostServiceImpl implements PostService {
     @Autowired
     private CPostRepository cpostRepository;
     @Autowired
-    private UtilityRepository utilityRepository;
+    private UtilityService utilityService;
 
 
     @Override
     @Transactional(readOnly = true)
-    public List<PostResult> findAll() {
-        List<Post> entities = postRepository.findAll();
-       return List<PostResult> dtoList = postMapper.toDTOList(entities);
+    public List<PostResult> findAll(Pageable pageable) {
+        List<Post> entities = postRepository.findAll(pageable);
+        List<PostResult> dtoList = postMapper.toDTOList(entities);
+        utilityService.fin
     }
 
 
