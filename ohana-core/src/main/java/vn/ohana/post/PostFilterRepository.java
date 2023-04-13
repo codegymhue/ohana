402dbe6b9469e41f2be454cc36a35c92ad1571fa
nguyennp.htra.031004@gmail.com <nguyennp.htra.031004@gmail.com>
2023-04-13 17:03:49 +0700

    search trend, seach homepage

--- ohana-core/src/main/java/vn/ohana/post/PostFilterRepository.java
+++ ohana-core/src/main/java/vn/ohana/post/PostFilterRepository.java
@@ -73,6 +73,12 @@ public interface PostFilterRepository extends JpaRepository<Post, Long>, JpaSpec
 
             if (filter.getLocationFilter() != null) {
 
+                if (filter.getLocationFilter().getLine1() != null) {
+                    Expression<String> expressionJSON = criteriaBuilder.function("JSON_EXTRACT", String.class, root.get("location"), criteriaBuilder.literal("$.line1"));
+                    Predicate line1Equal = criteriaBuilder.equal(expressionJSON, filter.getLocationFilter().getLine1());
+                    predicateList.add(line1Equal);
+
+                }
                 if (filter.getLocationFilter().getProvinceId() != null) {
                     Expression<String> expressionJSON = criteriaBuilder.function("JSON_EXTRACT", String.class, root.get("location"), criteriaBuilder.literal("$.provinceId"));
                     Predicate provinceIdEqual = criteriaBuilder.equal(expressionJSON, filter.getLocationFilter().getProvinceId());
