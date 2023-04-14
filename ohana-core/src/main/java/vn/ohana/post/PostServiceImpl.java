5671c7ebc74207d18b4283134369ead22bcac272
smeenguyen <nguyenvanquanghuy99@gmail.com>
2023-04-15 02:55:46 +0700

    them utility khi update status post

--- ohana-core/src/main/java/vn/ohana/post/PostServiceImpl.java
+++ ohana-core/src/main/java/vn/ohana/post/PostServiceImpl.java
@@ -115,7 +115,7 @@ public class PostServiceImpl implements PostService {
     public PostResult updateStatusById(PostUpdateParam postUpdateParam) {
         Post post = findById(postUpdateParam.getId());
         postMapper.transferFields(postUpdateParam,post,true);
-        return postMapper.toDTO(post);
+        return addPostResultUtilities(post,post.getUtilities());
     }
 
     @Override
