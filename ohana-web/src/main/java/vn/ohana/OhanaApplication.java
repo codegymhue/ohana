4812033be3fe5a1d8fa6e245239a47bcb79993a1
TrongAnh16 <anhtrduong@gmail.com>
2023-05-17 12:10:00 +0700

    fix deploy uncompleted

--- ohana-web/src/main/java/vn/ohana/OhanaApplication.java
+++ ohana-web/src/main/java/vn/ohana/OhanaApplication.java
@@ -3,10 +3,17 @@ package vn.ohana;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication(scanBasePackages = {"vn.ohana","vn.rananu"})
+@SpringBootApplication(scanBasePackages = {"vn.ohana", "vn.rananu"})
 @EntityScan("vn.ohana.entities")
-public class OhanaApplication {
+public class OhanaApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(OhanaApplication.class);
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(OhanaApplication.class, args);
     }
